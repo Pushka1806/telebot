c5a5efbb37ce5b863dd3ec50a870949e81fb59e4
Pushka1806 <artur.levchuk17@gmail.com>
2020-12-28 23:04:10 +0300

    запуск1

--- bot.py
+++ bot.py
@@ -1,86 +1,140 @@
 import telebot
-from db import database_sqlite
 
+from db.database_sqlite import *
+from config import *
 from telebot import types
 
-bot = telebot.TeleBot("1400204569:AAE41mIqzjsH6JQHmdg2ZWW-EODWgvOUKjI")
+bot = telebot.TeleBot(TOKEN)
+dima_id = 665665811
+artem_id = 800987769
+artur_id = 580910189
 
-name = " "
-surname = " "
-age = 0
-user_id = -1
-score = 0
+_doc = False
+_error = False
+error_count = 0
+
+
+def extract_unique_code(text):
+    # Extracts the unique_code from the sent /start command.
+    return text.split()[1] if len(text.split()) > 1 else None
 
 
 @bot.message_handler(commands=['start'])
+def first_massage(message):
+    unique_code = extract_unique_code(message.text)
+    print(type(unique_code))
+    print(type(message.from_user.id))
+    print(unique_code == message.from_user.id)
+    if unique_code and int(unique_code) != message.from_user.id:
+        value = 200
+        multiple_score(user_id=unique_code, value=value)
+        add_referral(user_id=unique_code)
+        bot.send_message(unique_code, f"Вам было зачислено {value} баллов ")
+
+    bot.send_message(message.from_user.id, "Привет, это тестовый телеграм бот")
+    bot.send_message(message.from_user.id, "Начнём")
+    bot.send_message(message.from_user.id, "Нажми на /menu")
+
+
+@bot.message_handler(commands=['menu'])
 def menu(message):
-    global user_id
-    user_id = message.from_user.id
-    print(user_id)
-    bot.reply_to(message, "Выбери вариант из меню")
+    global _doc
+    global _error
+
+    _doc = False
+    _error = False
+    # user_id = message.from_user.id
+    print(message.from_user.id)
+
+    init_db()
+    print(44444444)
+    if cheсk_id(user_id=message.from_user.id) == 0:
+        add_user(user_id=message.from_user.id, name=message.from_user.first_name, surname=message.from_user.last_name,
+                 score=0)
     menu1 = types.InlineKeyboardMarkup()
-    key_reg = types.InlineKeyboardButton(text='Регистрация', callback_data='reg')
-    menu1.add(key_reg)
+    key_score = types.InlineKeyboardButton(text='Ваши баллы', callback_data='score')
+    key_document = types.InlineKeyboardButton(text='Прикрепить документ\n или фотографию', callback_data='document')
+    key_error = types.InlineKeyboardButton(text='Сообщить об ошибке', callback_data='error')
+    key_price = types.InlineKeyboardButton(text='Prices', callback_data='price')
+    key_loyalty = types.InlineKeyboardButton(text='Лояльность', callback_data='loyalty')
+    menu1.add(key_price)
+    menu1.add(key_loyalty)
+    menu1.add(key_document)
+    menu1.add(key_score)
+    menu1.add(key_error)
     bot.send_message(message.chat.id, text="Меню", reply_markup=menu1)
 
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    global _error
+    if _error:
+        bot.send_message(message.from_user.id, "Спасибо, ваше сообщение будет отправлено автору")
+        bot.forward_message(artur_id, message.chat.id, message.message_id)
+        multiple_score(user_id=message.from_user.id, value=100)
+        bot.send_message(message.from_user.id, "Вам было зачислено 100 баллов ")
+        bot.send_message(message.from_user.id, "Выберите меню /menu")
+
+    else:
+
+        bot.send_message(message.from_user.id, "Выберите меню /menu")
 
 
-def reg_name(message):
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, "Фамилия?")
-    bot.register_next_step_handler(message, reg_surname)
+@bot.message_handler(func=lambda _error: True, content_types=['document', 'photo'])
+def komp(message):
+    global _doc
+    if _error == True:
+        bot.send_message(message.from_user.id, "Спасибо, ваше сообщение будет отправлено автору")
+        bot.forward_message(artur_id, message.chat.id, message.message_id)
+        multiple_score(user_id=message.from_user.id, value=100)
+        bot.send_message(message.from_user.id, 'Выберите /menu')
 
+    elif _doc:
+        bot.forward_message(artem_id, message.chat.id, message.message_id)
+        multiple_score(user_id=message.from_user.id, value=100)
+        bot.send_message(message.from_user.id, "100 баллов зачислено")
+        bot.send_message(message.from_user.id, 'Выберите /menu')
+        _doc = False
 
-def reg_surname(message):
-    global surname
-    surname = message.text
-    bot.send_message(message.from_user.id, "Сколько лет назад вылупился?")
-    bot.register_next_step_handler(message, reg_age)
-
-
-def reg_age(message):
-    global age
-
-    try:
-        age = int(message.text)
-        bot.send_message(message.from_user.id, "Отлично")
-    except Exception:
-        bot.send_message(message.from_user.id, "Цифры плиз")
-        bot.register_next_step_handler(message, reg_age)
-
-    keyboard = types.InlineKeyboardMarkup()
-    key_yes = types.InlineKeyboardButton(text='YES', callback_data='yes')
-    keyboard.add(key_yes)
-    key_no = types.InlineKeyboardButton(text='NO', callback_data='no')
-    keyboard.add(key_no)
-
-    bot.send_message(message.from_user.id,
-                     text='Информаци верна?\n Имя {name}\n Фамилия {surname}\n Возраст {age}'.format(name=name,
-                                                                                                     surname=surname,
-                                                                                                     age=age),
-                     reply_markup=keyboard)
-    return 0
+    else:
+        bot.send_message(message.from_user.id, "Чтобы отправить файл, выберите пункт меню /menu")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "yes":
-        bot.send_message(call.from_user.id, 'Привет {name}'.format(name=name))
-        search_or_save_users(user_id, name, surname, age, score)
-    elif call.data == "no":
-        bot.send_message(call.from_user.id, "Давай по новой")
-        bot.send_message(call.from_user.id, "Дороу.Как звать это чудо?")
-        bot.register_next_step_handler(call.message, reg_name)
-    elif call.data == "reg":
-        bot.send_message(call.from_user.id, "Поихалы")
-        print(bot.ef)
-        bot.send_message(call.from_user.id, "Дороу.Как звать это чудо?")
-        bot.register_next_step_handler(call.message, reg_name)
+    if call.data == "score":
+        bot.send_message(call.from_user.id, "Количество баллов:")
+        bot.send_message(call.from_user.id, check_score(user_id=call.from_user.id))
+        bot.send_message(call.from_user.id, "Для продолжения введите /menu")
+    elif call.data == "document":
+        global _doc
+        _doc = True
+        bot.send_message(call.from_user.id, "В описании можете добавить информацию к редактированию")
+        bot.send_message(call.from_user.id, "Для отмены выберите /menu")
+    elif call.data == "price":
+        bot.send_message(call.from_user.id,
+                         "Печать за страницу:       15к\nC вашими листами: 10к\nВ разделе Лояльность можете ознакомиться со списком скидок")
+        bot.send_message(call.from_user.id, "Для продолжения введите /menu")
+
+    elif call.data == "error":
+        global _error
+        _error = True
+        global error_count
+        error_count += 1
+        bot.send_message(call.from_user.id, 'Номер ошибки {error}'.format(error=error_count))
+        bot.send_message(call.from_user.id,
+                         "Раскажи мне об возникшей ошибке или прикрепи скриншот,в описании укажи номер ошибки")
+        bot.send_message(call.from_user.id, "Для отмены выберите /menu")
+    elif call.data == "loyalty":
+        loyal_url = types.InlineKeyboardMarkup()
+        key_url = types.InlineKeyboardButton(text='Получить ссылку', callback_data='_loyal_url')
+        loyal_url.add(key_url)
+        bot.send_message(call.from_user.id, text="Ссылка", reply_markup=loyal_url)
+
+        bot.send_message(call.from_user.id, "Для продолжения введите /menu")
+    elif call.data == "_loyal_url":
+        bot.send_message(call.from_user.id, "Отправьте ссылку другу и при его регистрации получите баллы")
+        bot.send_message(call.from_user.id, 't.me/test_shrek_bot?start=580910189')
 
 
-bot.polling()
+bot.infinity_polling()
--- db/database_sqlite.py
+++ db/database_sqlite.py
@@ -1,12 +1,120 @@
 import sqlite3
 
-with sqlite3.connect('db/database.db')as db:
-    cursor = db.cursor()
+
+def ensure_connection(func):
+    def inner(*args, **kwargs):
+        with sqlite3.connect('db/database.db') as cons:
+            res = func(*args, conn=cons, **kwargs)
+        return res
+
+    return inner
 
 
-    def search_or_save_users(user_id, name, surname, age, score):
-        query = """ CREATE TABLE IF NOT EXISTS users (user_id INTEGER, name TEXT,surname TEXT, age INT, score INT) """
-        # query = """ INSERT INTO expenses (id,name) VALUES (1,'Коммуналка') """
-        cursor.execute(query)
-        query1 = """INSERT INTO users VALUES(user_id,name,surname,age,score) """
-        cursor.execute(query1)
+@ensure_connection
+def init_db(conn, force: bool = False):
+    c = conn.cursor()
+
+    if force:
+        c.execute('DROP TABLE IF EXISTS users')
+        c.execute('DROP TABLE IF EXISTS partners')
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+        id          INTEGER PRIMARY KEY,
+        user_id     INTEGER,
+        name        TEXT,
+        surname     TEXT,
+        score       INTEGER
+        )
+        """)
+    c.execute("""
+            CREATE TABLE IF NOT EXISTS partners(
+            id_user         INTEGER PRIMARY KEY,
+            score_referrals INTEGER
+            
+            )
+            """)
+
+
+@ensure_connection
+def cheсk_id(conn, user_id):  # просмотр: записан ли пользователдь в БД
+    print(777777777777)
+    c = conn.cursor()
+    c.execute(
+        'SELECT user_id FROM users WHERE user_id = ?', (user_id,)
+    )
+    print(888888888)
+    one_result = c.fetchone()
+    if one_result == None:
+        return 0
+    else:
+        return 1
+
+
+
+@ensure_connection
+def add_user(conn, user_id: int, name: str, surname: str, score: int):
+    c = conn.cursor()
+    c.execute(
+        'INSERT INTO users (user_id,name,surname,score) VALUES(?,?,?,?)', (user_id, name, surname, score))
+    c.execute(
+        """INSERT INTO partners (score_referrals) VALUES(0)"""
+    )
+    conn.commit()
+
+
+@ensure_connection
+def add_referral(conn, user_id: int):  # добавление одного реферала в таблицу
+    c = conn.cursor()
+    score = get_referrals_score(user_id=user_id)
+    new_score = score[0] + 1
+    c.execute(
+        f"""INSERT INTO partners (score_referrals) VALUES('{new_score}')""")
+    conn.commit()
+
+
+@ensure_connection
+def get_id(conn, user_id: int):
+    c = conn.cursor()
+    c.execute(
+        'SELECT id FROM users WHERE user_id = ?', (user_id,)
+    )
+    result = c.fetchone()
+    return result
+
+
+@ensure_connection
+def get_referrals_score(conn, user_id):
+    new_id = get_id(user_id=user_id)
+    print(new_id)
+    c = conn.cursor()
+    c.execute(
+        f"""SELECT score_referrals FROM partners JOIN users
+            ON partners.id_user = users.id
+         WHERE id =  '{new_id[0]}'"""
+    )
+    result = c.fetchone()
+    print(result)
+    return result
+
+
+@ensure_connection
+def check_score(conn, user_id: int):
+    c = conn.cursor()
+    c.execute(
+        'SELECT score FROM users WHERE user_id = ?', (user_id,)
+    )
+    result = c.fetchone()
+    return result
+
+
+@ensure_connection
+def multiple_score(conn, user_id: int, value: int):
+    _score = check_score(user_id=user_id)
+    new_score = _score[0]
+    print(user_id)
+    new_score += value
+    c = conn.cursor()
+    c.execute(
+        f"""UPDATE users SET score = '{new_score}' WHERE user_id ='{user_id}'"""
+    )
+    conn.commit()
